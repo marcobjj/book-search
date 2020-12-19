08cb3273eae69a3ad67719c2d14f122077cff89a
Marco Evangelista <marcobjj@users.noreply.github.com>
2020-12-18 23:58:03 -0800

    removes old import

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 import {ADD_USER} from '../utils/mutations';
 import { useMutation } from '@apollo/react-hooks';
